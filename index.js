bcd01e93d4eb61cb8d8167fc7bc480d7e8788a0c
Ricardo Akira Nagaishi <r.a.nagaishi@gmail.com>
2018-09-11 11:05:04 -0300

    new version of index.js

--- index.js
+++ index.js
@@ -1,115 +1,100 @@
-/*Classes*/
-class Address {
-    constructor(type) {
-        this.type = type;
-        this.tags = [];
-        this.addr = "";
-    }
-    addTag(tag) {
-        return this.tags.push(tag);
-    }
-}
+const fs = require('fs'); 
+const parse = require ('csv-parse');
+const email_validator = require("email-validator");
+const phoneUtil = require ('google-libphonenumber').PhoneNumberUtil.getInstance();
 
-class Info {
-    constructor (fullname, eid) {
-        this.fullname = fullname;
-        this.eid = eid;
-        this.classes = [];
-        this.addresses = [];
-        this.invisible = false;
-        this.see_all = false;
-    }
+var input_file = 'input.csv';
+var tags = [];
+var header_processed = false;
+var addresses_list = [];
+var output = {};
 
-    addClass (clss) {
-        clss = clss.replace(/[\"]/, "");
-        clss = clss.trim();
-        if (clss != ""){
-            this.classes.push(clss);
-        }
-    }
-
-    addAddress (type, tags, addr) {
-        const address = new Address(type);
-
-        for(var i = 0; i < tags.length; ++i) {
-            address.addTag(tags[i])
-        }
-
-        if(type == "email") {
-            addr = veryfyAndFormatEmail(addr);
-        }
-        else if(type == "phone") {
-            addr = verifyAndFormatTelephoneNumber(addr);
-        }
-        var addNew = true;
-        if(addr != ""){
-
-            for (var i = 0; i < this.addresses.length; ++i) {
-                if (this.addresses[i].type == type && this.addresses[i].addr == addr) {
-                    addNew = false;
-
-                    for (var j = 0; j < tags.length; ++j) {
-                        this.addresses[i].tags.push(tags[j]);
-                    }
-                    break;
-                }
-            }
-
-            if (addNew == true) {
-                address.addr = addr;
-                this.addresses.push(address);
-            }
-        }
-    }
-
-    setBooleanField (field, value) {
-        if (value != "") {
-            value = verifyAndFormatBooleanFields(value);
-            if (field == "invisible" && value) {
-                this.invisible = value;
-            }
-            else if (field == "see_all") {
-                this.see_all = value;
-            }
-        }
-    }
-}
-/*End classes*/
-
-/*Auxiliar Functions*/
-//Verify if email is valid, cannot have more than one @ and need to finish with .com or .com.xx. Otherwise returns empty string
-function veryfyAndFormatEmail(email){
-    var fields = email.split("@")
-    if(fields.length != 2) {
-        return ""
-    }
-    var sulfix = fields[1].split(".");
-    if(sulfix.length >= 2){
-        if(sulfix[1] == "com"){
-            return email
-        }
-    }
-    else{
-        return "";
+function validate_email(email) {
+    if (email_validator.validate(email)) {
+        return email;
     }
     return "";
 }
 
-//Verify if is valid phone number, if not return empty string, if yes group the numbers and add 55 in the beginning
-function verifyAndFormatTelephoneNumber(number){
+function validate_phone(phone) {
     
-    number = number.replace(/[\s+()]/g, '');
-    
-    if((number.length==10  && number[2] != '9')|| (number.length==11 && number[2] == '9')) {
-        number = "55"+number;
-    }
-    else{
+    try {
+        const number = phoneUtil.parseAndKeepRawInput(phone, 'BR');
+
+        if (phoneUtil.isValidNumber(number) && phoneUtil.isPossibleNumber(number)) {
+            return number.getCountryCode().toString()+number.getNationalNumber().toString();
+        }
+        return "";
+
+    } catch (error) {
         return "";
     }
-    return number;
 }
 
-function verifyAndFormatBooleanFields(value){
+function validate_address(address, index) {
+
+    var address_info  = {
+        "type": "",
+        "tags": [],
+        "address" : ""
+    }
+    for (var i = 0; i < addresses_list.length; i++) {
+        var addr = "";
+        if (addresses_list[i]["index"] === index) {
+            if (addresses_list[i]["type"] === "email") {
+                addr = validate_email(address);
+            }
+            else if (addresses_list[i]["type"] === "phone") {
+                addr = validate_phone(address);
+            }
+
+            if (addr !== "") {
+                address_info["type"] = addresses_list[i]["type"];
+                address_info["tags"] = addresses_list[i]["tags"];
+                address_info["address"] = addr;
+            }
+        }
+    }
+
+    return address_info;
+}
+
+function split_info_field (info) {
+
+    if (info.includes("/")) {
+        info = info.split("/")
+    }
+    else if (info.includes(",")) {
+        info = info.split(",")
+    }
+    else {
+        info = info.split()
+    }
+
+    for (var i = 0; i < info.length; i++) {
+        info[i] = info[i].trim();
+    }
+    return info;
+}
+
+function get_address_type_and_tags(addr, index) {
+    addr = addr.split(" ");
+
+    addr_info = {
+        "index":index,
+        "type": addr[0],
+        "tags": []
+    }
+
+    for (var i = 1; i < addr.length; i++) {
+        addr[i] = addr[i].replace(",", "");
+        addr_info["tags"].push(addr[i]);
+    }
+
+    return addr_info;
+}
+
+function verify_format_boolean_fields(value){
     if(value == "yes" || value == "1") {
         return true;
     }
@@ -119,135 +104,94 @@ function verifyAndFormatBooleanFields(value){
     return false;
 }
 
-//Create string in json format
-function formatJsonString(jsonDict, header) {
+function process_row(row) {
 
-    let keys = Object.keys(jsonDict);
-    var jsonString = "[";
-
-    for (var i = 0; i < keys.length; ++i) {
-
-        var key = keys[i];
-        jsonString += "{\"" + header[0] + "\":\"" + jsonDict[key].fullname[0] + "\",";
-        jsonString += "\"" + header[1] + "\":\"" + jsonDict[key].eid[0] + "\",";
-        if (jsonDict[key].classes.length > 1){
-            jsonString += "\"classes\":[";
-            let size = jsonDict[key].classes.length;
-            for (var j = 0; j < size; ++j) {
-                jsonString += "\""+ jsonDict[key].classes[j]+"\""
-                if (j < size-1){
-                    jsonString += ", ";
-                }
-            }
-            jsonString += "],";
-        }
-        else {
-            jsonString += "\"classes\":" + "\""+jsonDict[key].classes[0] + "\",";
-        }
-        jsonString += "\"addresses\":[";
+    if (!header_processed) {
+        tags = row
         
-        let size = jsonDict[key].addresses.length;
-        for (var j = 0; j < size; ++j) {
-            jsonString += "{\"type\":\""+ jsonDict[key].addresses[j].type+"\",";
-            jsonString += "\"tags\":[";
-            for (var k = 0; k < jsonDict[key].addresses[j].tags.length; ++k) {
-                jsonString += "\""+ jsonDict[key].addresses[j].tags[k]+"\"";
-                if (k < jsonDict[key].addresses[j].tags.length-1) {
-                    jsonString += ",";
-                }
-            }
-            jsonString += "],\"address\":\""+ jsonDict[key].addresses[j].addr+"\"}"
-            if (j < size-1) {
-                jsonString += ",";
+        for (var i = 2; i < row.length; i++) {
+            if (row[i] !== "class") {
+                addresses_list.push(get_address_type_and_tags(row[i], i))
             }
         }
-        jsonString += "],";
-
-        jsonString += "\"invisible\":" + jsonDict[key].invisible+",";
-        jsonString += "\"see_all\":" + jsonDict[key].see_all+"}";
-
-        if (i < keys.length-1) {
-            jsonString += ",";
-        }
-    }
-    jsonString += "]";
-
-    return jsonString;
-}
-/*End Auxiliar Functions*/
-
-/*Main program*/
-const fs = require('fs')
-
-const path = __dirname + '/input.csv'
-
-let content = fs.readFileSync(path, 'utf-8').toString().split('\n')
-
-var header = content[0].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-
-var jsonDict = {}
-
-for (var i = 4; i < 10; ++i){
-    header[i] = header[i].split(" ");
-    for(var j = 0; j < header[i].length; ++j){
-        header[i][j] = header[i][j].replace(/[,+ +"+]/, "");
-    }
-}
-
-for(var i = 1; i < content.length; ++i) {
-
-    var line = content[i].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-    
-    //split each field by ',' or '/'
-    for(var j = 0; j < line.length; ++j) {
-        line[j] = line[j].split(/[,/]/);
-    }
-    
-    if(jsonDict[line[0]] == null) {
-        //Store info in class Info
-        const info = new Info(line[0], line[1]);
-    
-        for(var k = 0; k < line[2].length; ++k) {
-            info.addClass(line[2][k]);
-        }
-        for(var k = 0; k < line[3].length; ++k) {
-            info.addClass(line[3][k]);
-        }
-        for(var k = 4; k < 10; ++k){
-            let tags = [];
-            for(var l=1; l < header[k].length; ++l) {
-                tags.push(header[k][l]);
-            }
-            for(var l=0; l < line[k].length; ++l) {
-                info.addAddress(header[k][0], tags, line[k][l]);
-            }
-        }
-
-        info.setBooleanField("invisible", line[10]);
-        info.setBooleanField("see_all", line[11]);
-        jsonDict[line[0]] = info;
+        header_processed = true;
     }
     else {
-        for(var k = 0; k < line[2].length; ++k) {
-            jsonDict[line[0]].addClass(line[2][k]);
-        }
-        for(var k = 0; k < line[3].length; ++k) {
-            jsonDict[line[0]].addClass(line[3][k]);
-        }
-        for(var k = 4; k < 10; ++k){
-            let tags = [];
-            for(var l=1; l < header[k].length; ++l) {
-                tags.push(header[k][l]);
+        var person = {
+            "fullname":row[0],
+            "eid":row[1],
+            "classes":[],
+            "addresses":[],
+            "invisible":false,
+            "see_all":false
+        };
+
+        for (var i = 2; i < row.length; i++) {
+            if (tags[i] === "class" && row[i] !== "") {
+                var classes = split_info_field(row[i])
+                
+                person["classes"] = person["classes"].concat(classes);
             }
-            for(var l=0; l < line[k].length; ++l) {
-                jsonDict[line[0]].addAddress(header[k][0], tags, line[k][l]);
+            else if (tags[i] == "invisible") {
+                person["invisible"] = verify_format_boolean_fields(row[i]);
+            }
+            else if (tags[i] == "see_all") {
+                person["see_all"] = verify_format_boolean_fields(row[i]);
+            }
+            else {
+                var addr_list = split_info_field(row[i]);
+
+                for (var j = 0; j < addr_list.length; j++) {
+                    var addr = validate_address(addr_list[j], i);
+                    if (addr["address"] !== "") {
+                        var isDuplicated = false;
+                        for (var index in person["addresses"]) {
+                            if(person["addresses"][index]["address"] === addr["address"]) {
+                                person["addresses"][index]["tags"] = person["addresses"][index]["tags"].concat(addr["tags"]);
+                                isDuplicated = true;
+                                break;
+                            }
+                        }
+                        if (!isDuplicated) {
+                            person["addresses"].push(addr);
+                        }
+                    }
+                }
             }
         }
-        jsonDict[line[0]].setBooleanField("invisible", line[10]);
-        jsonDict[line[0]].setBooleanField("see_all", line[11]);
+        if (person["eid"] in output) {
+            output[person["eid"]]["classes"] = output[person["eid"]]["classes"].concat(person["classes"]);
+            output[person["eid"]]["addresses"] = output[person["eid"]]["addresses"].concat(person["addresses"]);
+            output[person["eid"]]["invisible"] = output[person["eid"]]["invisible"] || person["invisible"];
+            output[person["eid"]]["see_all"] = output[person["eid"]]["see_all"] || person["see_all"];
+        }
+        else {
+            output[person["eid"]] = person;
+        }
     }
 }
+var parser = parse({delimiter: ','}, function (err, data) {
+    
+    data.forEach(process_row)
 
-//Formatting json string and creating output file
-let jsonString = formatJsonString(jsonDict, header);
-fs.writeFile("output.json", jsonString, (error) => {});
+    var json_array  = []
+
+    for (var key in output) {
+        if (output[key]["classes"].length == 1){
+            output[key]["classes"] = output[key]["classes"][0];
+        }
+        json_array.push(output[key])
+    }
+
+    const json = JSON.stringify(json_array);
+
+    console.log (json);
+
+    fs.writeFile("output.json", json, (error) => {
+        if (error !== null) {
+            console.log(error)
+        }
+    });
+});
+
+fs.createReadStream(input_file).pipe(parser);
